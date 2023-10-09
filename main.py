c1c4e1fd0291e511eb7e4d771ffbd3142403c496
Farukeness <80718763+Farukeness@users.noreply.github.com>
2023-10-09 13:21:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("merhaba")
+import random 
+karakterler = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sifre_uzunlugu = int(input("Şifrenizin kaç karakterden oluşmasını istersiniz:"))
+
+sifre = ""
+for i in range(sifre_uzunlugu):
+    sifre += random.choice(karakterler)
+    
+print(sifre)
