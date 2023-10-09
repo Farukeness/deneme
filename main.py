f3980cba763fd520c1d38f7453993f1717e66b91
Farukeness <80718763+Farukeness@users.noreply.github.com>
2023-10-09 21:28:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("merhaba dünya")
+import random 
+karakterler = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sifre_uzunlugu = int(input("Şifreniz Kaç karakter uzunluğunda olsun: "))
+
+sifre = ""
+for i in range(sifre_uzunlugu):
+    sifre += random.choice(karakterler)
+
+print(sifre)
